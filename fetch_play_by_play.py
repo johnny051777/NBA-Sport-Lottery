649e999ac28f0ce0ffb5963bb28c457bb99124a0
johnnylin01 <linjohnny500@gmail.com>
2025-05-15 21:12:56 +0800

    modify

--- fetch_play_by_play.py
+++ fetch_play_by_play.py
@@ -1,7 +1,8 @@
 from nba_api.live.nba.endpoints import scoreboard, playbyplay
 import json
 
-# 取得今日比賽
-games = scoreboard.ScoreBoard().get_dict()['scoreboard']['games']
+# 取得今日比賽資訊
+sb = scoreboard.ScoreBoard()
+games = sb.get_dict()["scoreboard"]["games"]
 
-print(json.dumps(games,indent=4))
+print(json.dumps(games[1],indent=4))
